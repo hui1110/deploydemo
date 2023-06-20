b5ba404a5330a3137f32b289562c10332a877cc0
hui1110 <hui1110rui@163.com>
2023-06-20 15:23:01 +0800

    add  mysql test

--- src/main/java/com/hui/deploydemo/DeploydemoApplication.java
+++ src/main/java/com/hui/deploydemo/DeploydemoApplication.java
@@ -1,13 +1,28 @@
 package com.hui.deploydemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class DeploydemoApplication {
+public class DeploydemoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeploydemoApplication.class, args);
 	}
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @Override
+    public void run(String... args) throws Exception {
+        String sql = "INSERT INTO users (fullname, email, password) VALUES (?, ?, ?)";
+
+        int result = jdbcTemplate.update(sql, "Ravi Kumar", "ravi.kumar@gmail.com", "ravi2021");
+
+        if (result > 0) {
+            System.out.println("A new row has been inserted.");
+        }
+    }
 }
