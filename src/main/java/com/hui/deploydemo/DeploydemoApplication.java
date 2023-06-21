d9d3ad829b9a285ce7a87da22a85f173a5205af2
Yonghui Ye <58474919+hui1110@users.noreply.github.com>
2023-06-21 15:13:57 +0800

    Update DeploydemoApplication.java

--- src/main/java/com/hui/deploydemo/DeploydemoApplication.java
+++ src/main/java/com/hui/deploydemo/DeploydemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class DeploydemoApplication implements CommandLineRunner {
